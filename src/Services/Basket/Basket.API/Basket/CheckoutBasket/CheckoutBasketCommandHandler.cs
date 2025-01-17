efb3d674e44369080376fd1eaca10f4503d8067f
Mohammad Ajijul Hoque <ajijul.hoque@fefundinfo.com>
2025-01-17 08:52:00 +0530

    Add messaging project and integrate RabbitMQ
    Added new BuildingBlocks.Messaging project with MassTransit.RabbitMQ targeting .NET 8.0. Introduced BasketCheckoutEvent and base IntegrationEvent. Added AddMessageBroker extension method for MassTransit configuration. Updated Basket.API to reference new messaging project, implemented CheckoutBasketCommandHandler, and added CheckoutBasketEndpoint. Created BasketCheckoutDto. Updated Program.cs to configure message broker and added RabbitMQ settings to appsettings.json. Updated solution file to include new project.

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Basket.API.Dtos;
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Basket.API.Basket.CheckoutBasket
+{
+    public record CheckoutBasketCommand(BasketCheckoutDto BasketCheckoutDto)
+        : ICommand<CheckoutBasketResult>;
+    public record CheckoutBasketResult(bool IsSuccess);
+
+    public class CheckoutBasketValidator : AbstractValidator<CheckoutBasketCommand>
+    {
+        public CheckoutBasketValidator()
+        {
+            RuleFor(x => x.BasketCheckoutDto).NotNull().WithMessage("BasketCheckoutDto can't be null");
+            RuleFor(x => x.BasketCheckoutDto.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    public class CheckoutBasketCommandHandler(IBasketRepository repository, IPublishEndpoint publishEndpoint) : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+    {
+        public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+        {
+            var basket = await repository.GetBasket(command.BasketCheckoutDto.UserName, cancellationToken).ConfigureAwait(false);
+
+            if(basket is null)
+            {
+                return new CheckoutBasketResult(false);
+            }
+
+            var eventMessage = command.BasketCheckoutDto.Adapt<BasketCheckoutEvent>();
+            eventMessage.TotalPrice = basket.TotalPrice;
+
+            await publishEndpoint.Publish(eventMessage, cancellationToken).ConfigureAwait(false);
+
+            await repository.DeleteBasket(command.BasketCheckoutDto.UserName, cancellationToken).ConfigureAwait(false);
+
+            return new CheckoutBasketResult(true);
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using BuildingBlocks.Messaging.MassTransit;
+
 using Discount.Grpc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -42,6 +44,9 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
 
         return handler;
     });
+
+//Async Communication Services
+builder.Services.AddMessageBroker(builder.Configuration);
 
 // Cross-Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
